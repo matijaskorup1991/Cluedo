bfede808b70add583eb1a30832e267835f09b060
matijaskorup1991 <smatija1991@gmail.com>
2021-05-28 06:57:32 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+let $ = (el) => document.querySelector(el);
+let $$ = (el) => document.querySelectorAll(el);
+
 const suspectsArray = [];
 const weaponsArray = [];
 const roomsArray = [];
+
+function selectRandom(arr) {
+  const { floor, random } = Math;
+  return arr[floor(random() * arr.length)];
+}
+
+function pickMistery() {
+  let suspect = selectRandom(suspectsArray);
+  return {
+    firstName: suspect.firstName,
+    lastName: suspect.lastName,
+    weapon: selectRandom(weaponsArray).name,
+    room: selectRandom(roomsArray).name,
+  };
+}
+
+function revealMystery(obj) {}
